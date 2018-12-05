e5b6a5794d9aee7acc39e009bc9dbd2483eafb04
SolsticeDante <solsticedante@gmail.com>
2018-12-05 17:54:54 +0800

    added connectivity to firebase

--- Android App/3DPrinterMate (Backup)/app/src/main/java/com/example/zifang/a3dprintermate/MainActivity.java
+++ Android App/3DPrinterMate (Backup)/app/src/main/java/com/example/zifang/a3dprintermate/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.zifang.a3dprintermate;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private int counter = 5;
+
+    // References for Firebase connection
+    private DatabaseReference dr = FirebaseDatabase.getInstance().getReference();
+    private HashMap firebaseAllData;  // for storing all data from root. If time permits find a way to retrieve from child node "3D Printer Index" only?
+    private ArrayList printerIndexList = new ArrayList();  // for storing indices of all 3D printers
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -51,13 +75,33 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
 
+        // Retrieving data from Firebase once
+        // We retrieve all data from root, then the ArrayList that has all indices of 3D printers
+        // available.
+        dr.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        firebaseAllData = (HashMap) dataSnapshot.getValue();
+                        printerIndexList = (ArrayList) firebaseAllData.get("3D Printer Index");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
     }
 
     private void validate(String username){
-        if(username.equals("Admin")){
+        // If input a string value that matches index of any 3D printer --> log in success
+        if(printerIndexList.contains(username)){
             Intent intent = new Intent(this,SecondActivity.class);
+            intent.putExtra(getString(R.string.intent_key_printerIndex), username);
             startActivity(intent);
         }else{
+            // TO-DO: Create toast to show that wrong index has been inserted
             counter--;
 
             Info.setText("No. of attemps remaining: " + String.valueOf(counter));
--- Android App/3DPrinterMate (Backup)/app/src/main/java/com/example/zifang/a3dprintermate/SecondActivity.java
+++ Android App/3DPrinterMate (Backup)/app/src/main/java/com/example/zifang/a3dprintermate/SecondActivity.java
@@ -9,12 +9,37 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SecondActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+
+    // References for Firebase connection
+    private DatabaseReference dr = FirebaseDatabase.getInstance().getReference();
+    private String printerIndex;  // for saving index of printer
+    private HashMap firebaseAllData;  // for storing all data from root. If time permits find a way to retrieve from child node "3D Printer Index" only?
+    private HashMap printerData;  // for storing indices of all 3D printers
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +62,45 @@ public class SecondActivity extends AppCompatActivity implements NavigationView.
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new MainFragment()).commit();
         navigationView.setCheckedItem(R.id.nav_main);
+
+
+        // Index of 3D printer logged in is passed as intent method putExtra(). It is
+        // received here
+        Intent intent = getIntent();
+        printerIndex = intent.getStringExtra(getString(R.string.intent_key_printerIndex));
+
+        // constant listener for values for printer with index "printerIndex"
+        // note that firebase listeners bby default run in parallel with the app, so we don't
+        // have to put this under async task to have the listener constantly running
+        dr.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        firebaseAllData = (HashMap) dataSnapshot.getValue();
+                        printerData = (HashMap) firebaseAllData.get(printerIndex);
+
+                        // updates status display on fragment_main2.xml
+                        TextView statusView = findViewById(R.id.textView3);
+                        statusView.setText((String) printerData.get("Status"));
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
     }
 
     public void backtomain(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getString(R.string.intent_key_printerIndex),printerIndex);
         startActivity(intent);
     }
 
     public void backtosecond(){
         Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(getString(R.string.intent_key_printerIndex),printerIndex);
         startActivity(intent);
     }
 
